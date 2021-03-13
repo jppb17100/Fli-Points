a70f605ca4e15aad29043e4e8f06a4e5c3ff658c
davi <davi.amorim@flieger.software>
2021-03-13 08:55:13 -0300

    Update company.blade.php and web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Company\CompanyController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return  view('welcome');
 });
+
+Route::prefix('/company')->group(function(){
+
+    Route::get('/', [\App\Http\Controllers\Company\CompanyController::class, 'index']);
+    Route::get('/create', [\App\Http\Controllers\Company\CompanyController::class, 'create']); // ->name('')
+    Route::post('/create', [\App\Http\Controllers\Company\CompanyController::class, 'create']);
+    
+    //Route::get('/store', [\App\Http\Controllers\Company\CompanyController::class, 'store']);
+    //Route::post('/store', [\App\Http\Controllers\Company\CompanyController::class, 'store']);
+
+    //Route::get('/show', [\App\Http\Controllers\Company\CompanyController::class, 'show']);
+    Route::get('/edit/{id}', [\App\Http\Controllers\Company\CompanyController::class, 'edit']);
+    Route::post('/edit/{id}', [\App\Http\Controllers\Company\CompanyController::class, 'edit']);
+    
+    //Route::get('/update', [\App\Http\Controllers\Company\CompanyController::class, 'update']);
+    Route::get('/destroy/{id}', [\App\Http\Controllers\Company\CompanyController::class, 'destroy']);
+
+
+});
+
+//Route::resource('company', \App\Http\Controllers\Company\CompanyController::class);
